bbcec7466c1ef73b08436a421f5b3d9e98e23e5b
Vyacheslav Tabolin <vtabolin@mirantis.com>
2016-02-18 19:45:16 +0300

    Code clear for firewall driver
    Conflicts:
    	vmware_dvs/agent/firewalls/vcenter_firewall.py
    	vmware_dvs/utils/security_group_utils.py

--- vmware_dvs/agent/firewalls/vcenter_firewall.py
+++ vmware_dvs/agent/firewalls/vcenter_firewall.py
@@ -14,8 +14,7 @@
 #    under the License.
 
 from neutron.agent import firewall
-from neutron.common import constants
-from neutron.i18n import _LI, _LW
+from neutron.i18n import _LW
 from oslo_log import log as logging
 
 from vmware_dvs.common import config
@@ -34,8 +33,6 @@ class DVSFirewallDriver(firewall.FirewallDriver):
         self.networking_map = dvs_util.create_network_map_from_config(
             CONF.ml2_vmware)
         self.dvs_ports = {}
-        self.sg_rules = {}
-        self.sg_members = {}
         self._pre_defer_dvs_ports = None
         self.pre_sg_rules = None
         self.pre_sg_members = None
@@ -45,35 +42,41 @@ class DVSFirewallDriver(firewall.FirewallDriver):
 
     def prepare_port_filter(self, port):
         self.dvs_ports[port['device']] = port
-        LOG.info(_LI("Applied port %s for dvs port list"), port['id'])
         # Called for setting port in dvs_port_map
         self._get_dvs_for_port_id(port['id'],
                                   port['binding:vif_details']['dvs_port_key'])
 
     def apply_port_filter(self, port):
         self.dvs_ports[port['device']] = port
-        LOG.info(_LI("Applied port %s for dvs port list"), port['id'])
         # Called for setting port in dvs_port_map
         self._get_dvs_for_port_id(port['id'],
                                   port['binding:vif_details']['dvs_port_key'])
 
     def update_port_filter(self, port):
         self.dvs_ports[port['device']] = port
-        LOG.info(_LI("Updated port %s for dvs port list"), port['id'])
         # Called for setting port in dvs_port_map
         self._get_dvs_for_port_id(port['id'],
                                   port['binding:vif_details']['dvs_port_key'])
         self._apply_sg_rules_for_port(port)
 
-    def remove_port_filter(self, port):
-        self._remove_sg_from_dvs_port(port)
-        self.dvs_ports.pop(port['device'], None)
-        for port_set in self.dvs_port_map.values():
-            port_set.discard(port['id'])
+    def remove_port_filter(self, ports):
+        for port in ports:
+            self.dvs_ports.pop(port['device'], None)
+            for port_set in self.dvs_port_map.values():
+                port_set.discard(port['id'])
 
     @property
     def ports(self):
         return self.dvs_ports
+
+    @dvs_util.wrap_retry
+    def _apply_sg_rules_for_port(self, port):
+        # TODO(akamyshnikova): improve applying rules in case of agent restart
+        if port['security_group_rules']:
+            dvs = self._get_dvs_for_port_id(port['id'],
+                  port['binding:vif_details']['dvs_port_key'])
+            if dvs:
+                sg_util.update_port_rules(dvs, [port])
 
     def update_security_group_rules(self, sg_id, sg_rules):
         pass
@@ -84,27 +87,6 @@ class DVSFirewallDriver(firewall.FirewallDriver):
     def security_group_updated(self, action_type, sec_group_ids,
                                device_id=None):
         pass
-
-    @dvs_util.wrap_retry
-    def _apply_sg_rules_for_port(self, port):
-        dev = port['device']
-        sg_rules = 'security_group_rules'
-        known_ports = (set.union(*self.dvs_port_map.values())
-                       if self.dvs_port_map.values() else {})
-        for sg in port['security_groups']:
-            if sg in self.sg_rules.keys():
-                if (port['id'] not in known_ports or
-                        self.dvs_ports[dev][sg_rules] != self.sg_rules[sg]):
-                    port['security_group_rules'] = self.sg_rules[sg]
-        # TODO(akamyshnikova): improve applying rules in case of agent restart
-        if port['security_group_rules']:
-            if port['binding:vif_details'].get('dvs_port_key') is not None:
-                dvs = self._get_dvs_for_port_id(
-                    port['id'], port['binding:vif_details']['dvs_port_key'])
-            else:
-                dvs = self._get_dvs_for_port_id(port['id'])
-            if dvs:
-                sg_util.update_port_rules(dvs, [port])
 
     def _get_dvs_for_port_id(self, port_id, p_key=None):
         # Check if port is already known
@@ -147,97 +129,9 @@ class DVSFirewallDriver(firewall.FirewallDriver):
     def filter_defer_apply_on(self):
         if not self._defer_apply:
             self._pre_defer_dvs_ports = dict(self.dvs_ports)
-            self.pre_sg_members = dict(self.sg_members)
-            self.pre_sg_rules = dict(self.sg_rules)
             self._defer_apply = True
-
-    def _remove_unused_security_group_info(self):
-        """Remove any unnecessary local security group info.
-        """
-        dvs_ports = self.dvs_ports.values()
-
-        remote_sgs_to_remove = self._determine_remote_sgs_to_remove(
-            dvs_ports)
-
-        for ip_version, remote_sg_ids in remote_sgs_to_remove.iteritems():
-            self._clear_sg_members(ip_version, remote_sg_ids)
-
-        self._remove_unused_sg_members()
-
-        # Remove unused security group rules
-        for remove_group_id in self._determine_sg_rules_to_remove(
-                dvs_ports):
-            self.sg_rules.pop(remove_group_id, None)
-
-    def _determine_remote_sgs_to_remove(self, dvs_ports):
-        """Calculate which remote security groups we don't need anymore.
-
-        We do the calculation for each ip_version.
-        """
-        sgs_to_remove_per_ipversion = {constants.IPv4: set(),
-                                       constants.IPv6: set()}
-        remote_group_id_sets = self._get_remote_sg_ids_sets_by_ipversion(
-            dvs_ports)
-        for ip_version, remote_group_id_set in (
-                remote_group_id_sets.iteritems()):
-            sgs_to_remove_per_ipversion[ip_version].update(
-                set(self.pre_sg_members) - remote_group_id_set)
-        return sgs_to_remove_per_ipversion
-
-    def _get_remote_sg_ids_sets_by_ipversion(self, dvs_ports):
-        """Given a port, calculates the remote sg references by ip_version."""
-        remote_group_id_sets = {constants.IPv4: set(),
-                                constants.IPv6: set()}
-        for port in dvs_ports:
-            remote_sg_ids = self._get_remote_sg_ids(port)
-            for ip_version in (constants.IPv4, constants.IPv6):
-                remote_group_id_sets[ip_version] |= remote_sg_ids[ip_version]
-        return remote_group_id_sets
-
-    def _determine_sg_rules_to_remove(self, dvs_ports):
-        """Calculate which security groups need to be removed.
-
-        We find out by subtracting our previous sg group ids,
-        with the security groups associated to a set of ports.
-        """
-        port_group_ids = self._get_sg_ids_set_for_ports(dvs_ports)
-        return set(self.pre_sg_rules) - port_group_ids
-
-    def _get_sg_ids_set_for_ports(self, dvs_ports):
-        """Get the port security group ids as a set."""
-        port_group_ids = set()
-        for port in dvs_ports:
-            port_group_ids.update(port.get('security_groups', []))
-        return port_group_ids
-
-    def _clear_sg_members(self, ip_version, remote_sg_ids):
-        """Clear our internal cache of sg members matching the parameters."""
-        for remote_sg_id in remote_sg_ids:
-            if ip_version in self.sg_members[remote_sg_id]:
-                self.sg_members[remote_sg_id][ip_version] = []
-
-    def _remove_unused_sg_members(self):
-        """Remove sg_member entries where no IPv4 or IPv6 is associated."""
-        for sg_id in self.sg_members.keys():
-            sg_has_members = (self.sg_members[sg_id].get(constants.IPv4) or
-                              self.sg_members[sg_id].get(constants.IPv6))
-            if not sg_has_members:
-                del self.sg_members[sg_id]
-
-    def _get_remote_sg_ids(self, port, direction=None):
-        sg_ids = port.get('security_groups', [])
-        remote_sg_ids = {constants.IPv4: set(), constants.IPv6: set()}
-        for sg_id in sg_ids:
-            for rule in self.sg_rules.get(sg_id, []):
-                if not direction or rule['direction'] == direction:
-                    remote_sg_id = rule.get('remote_group_id')
-                    ether_type = rule.get('ethertype')
-                    if remote_sg_id and ether_type:
-                        remote_sg_ids[ether_type].add(remote_sg_id)
-        return remote_sg_ids
 
     def filter_defer_apply_off(self):
         if self._defer_apply:
             self._defer_apply = False
-            self._remove_unused_security_group_info()
             self._pre_defer_dvs_ports = None
