ce836f35850f7e03b4e52c4e6d32a97d00182adc
Vyacheslav Tabolin <vtabolin@mirantis.com>
2016-05-19 17:21:22 +0300

    Added support of enhanced RPC which scales better
    High amqp load can drop RabbitMQ HA replication. Patch uses more
    scalable RPC calls to reduse it.

--- vmware_dvs/agent/firewalls/dvs_securitygroup_rpc.py
+++ vmware_dvs/agent/firewalls/dvs_securitygroup_rpc.py
@@ -12,30 +12,58 @@
 #    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
 #    License for the specific language governing permissions and limitations
 #    under the License.
+
 from oslo_log import log as logging
 
+from neutron.agent import firewall
 from neutron.agent import securitygroups_rpc
-from neutron.i18n import _LI
+from neutron.i18n import _LI, _LW
+
+from vmware_dvs.utils.rpc_translator import update_rules
 
 LOG = logging.getLogger(__name__)
+
+sg_cfg = securitygroups_rpc.cfg.CONF.SECURITYGROUP
 
 
 class DVSSecurityGroupRpc(securitygroups_rpc.SecurityGroupAgentRpc):
 
-    @property
-    def use_enhanced_rpc(self):
-        if self._use_enhanced_rpc is None:
-            self._use_enhanced_rpc = False
-        return self._use_enhanced_rpc
+    def __init__(self, context, plugin_rpc,
+                 defer_refresh_firewall=False):
+        self.context = context
+        self.plugin_rpc = plugin_rpc
+        self.init_firewall(defer_refresh_firewall)
+
+    def init_firewall(self, defer_refresh_firewall=False):
+        firewall_driver = sg_cfg.firewall_driver or 'noop'
+        LOG.debug("Init firewall settings (driver=%s)", firewall_driver)
+        if not securitygroups_rpc._is_valid_driver_combination():
+            LOG.warn(_LW("Driver configuration doesn't match "
+                         "with enable_security_group"))
+        firewall_class = firewall.load_firewall_driver_class(firewall_driver)
+        self.firewall = firewall_class()
+        # The following flag will be set to true if port filter must not be
+        # applied as soon as a rule or membership notification is received
+        self.defer_refresh_firewall = defer_refresh_firewall
+        # Stores devices for which firewall should be refreshed when
+        # deferred refresh is enabled.
+        self.devices_to_refilter = set()
+        # Flag raised when a global refresh is needed
+        self.global_refresh_firewall = False
+        self._use_enhanced_rpc = None
 
     def prepare_devices_filter(self, device_ids):
         if not device_ids:
             return
         LOG.info(_LI("Preparing filters for devices %s"), device_ids)
 
-        devices = self.plugin_rpc.security_group_rules_for_devices(
-            self.context, list(device_ids))
-
+        if self.use_enhanced_rpc:
+            devices_info = self.plugin_rpc.security_group_info_for_devices(
+                self.context, list(device_ids))
+            devices = update_rules(devices_info)
+        else:
+            devices = self.plugin_rpc.security_group_rules_for_devices(
+                self.context, list(device_ids))
         self.firewall.prepare_port_filter(devices.values())
 
     def remove_devices_filter(self, device_ids):
@@ -51,7 +79,11 @@ class DVSSecurityGroupRpc(securitygroups_rpc.SecurityGroupAgentRpc):
             if not device_ids:
                 LOG.info(_LI("No ports here to refresh firewall"))
                 return
-        devices = self.plugin_rpc.security_group_rules_for_devices(
+        if self.use_enhanced_rpc:
+            devices_info = self.plugin_rpc.security_group_info_for_devices(
                 self.context, device_ids)
-
+            devices = update_rules(devices_info)
+        else:
+            devices = self.plugin_rpc.security_group_rules_for_devices(
+                self.context, device_ids)
         self.firewall.update_port_filter(devices.values())
