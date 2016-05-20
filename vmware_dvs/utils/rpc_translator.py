29acbf26bfde1c34932d24339b5c218c99ededae
Vyacheslav Tabolin <vtabolin@mirantis.com>
2016-05-20 17:33:55 +0300

    Small code refactoring for last commits

--- vmware_dvs/utils/rpc_translator.py
+++ vmware_dvs/utils/rpc_translator.py
@@ -16,33 +16,30 @@
 import copy
 
 
-def update_rules(enh_rpc):
-    sg_members = enh_rpc['sg_member_ips']
-    devices = enh_rpc['devices']
-    sg = enh_rpc['security_groups']
-    ret = copy.copy(devices)
-    for device in devices:
-        sg_dev = devices[device]['security_groups']
-        my_ip = devices[device]['fixed_ips']
-        for sg in sg_dev:
-            sg_rules = enh_rpc['security_groups'][sg]
-            for sgrule in sg_rules:
-                if 'remote_group_id' in sgrule:
-                    ret[device]['security_group_rules'].extend(
-                        build_rules_from_sg(sgrule, sg_members, sg, my_ip))
+def update_rules(devices_rules_info):
+    sg_members = devices_rules_info['sg_member_ips']
+    devices = devices_rules_info['devices']
+    result = copy.copy(devices)
+    for device, device_info in devices.items():
+        device_ips = device_info['fixed_ips']
+        for sg in device_info['security_groups']:
+            for sg_rule in devices_rules_info['security_groups'][sg]:
+                if 'remote_group_id' in sg_rule:
+                    result[device]['security_group_rules'].extend(
+                        build_rules_from_sg(sg_rule, sg_members, device_ips))
                 else:
-                    ret[device]['security_group_rules'].append(sgrule)
-    return ret
+                    result[device]['security_group_rules'].append(sg_rule)
+    return result
 
 
-def build_rules_from_sg(rule, sg_members, sg, my_ip):
+def build_rules_from_sg(rule, sg_members, device_ips):
     rules = []
-    for ips in sg_members[rule['remote_group_id']][rule['ethertype']]:
-        if ips not in my_ip:
+    for ip in sg_members[rule['remote_group_id']][rule['ethertype']]:
+        if ip not in device_ips:
             r_builder = copy.copy(rule)
             if rule['direction'] == 'ingress':
-                r_builder[u'source_ip_prefix'] = ips + '/32'
+                r_builder[u'source_ip_prefix'] = ip + '/32'
             else:
-                r_builder[u'dest_ip_prefix'] = ips + '/32'
+                r_builder[u'dest_ip_prefix'] = ip + '/32'
             rules.append(r_builder)
     return rules
